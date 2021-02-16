bd51c62d7bf99b9080dbb26768d473da5d92cdf3
Siarhei Khonski <armonius@mail.ru>
2021-02-17 00:36:54 +0300

    feat: write transform-array and test it

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Object.prototype.toString.call(arr) != '[object Array]') throw Error;
+  let newArr = arr.map(el => el === '--discard-next' || el === '--discard-prev' || el === '--double-next' || el === '--double-prev' ? el = '' : el);
+  arr.map((el, i) => {
+    if (el === '--discard-next') i != (arr.length - 1) && !(newArr[i + 1] ==='') ? newArr.splice(i + 1, 1, '') : 0;
+    if (el === '--discard-prev') i != 0 && !(newArr[i - 1] ==='') ? newArr.splice(i - 1, 1, '') : 0;
+    if (el === '--double-next') i != (arr.length - 1) && !(newArr[i + 1] ==='') ? newArr.splice(i, 1, arr[i + 1] ) : 0;
+    if (el === '--double-prev') i != 0 && !(newArr[i - 1] ==='') ? newArr.splice(i, 1, arr[i - 1]) : 0;
+  });
+  return newArr.filter(el => !(el === ''));
 };
