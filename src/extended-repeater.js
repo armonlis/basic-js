2985b86d6d1e65ee2c9979a58c46d78c6136dfbe
Siarhei Khonski <armonius@mail.ru>
2021-02-18 22:53:49 +0300

    feat: create and test extended-repeated

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,14 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (!(options.hasOwnProperty('repeatTimes'))) options.repeatTimes = 1; 
+  if (!(Number.isInteger(options.repeatTimes) && options.repeatTimes >= 0 )) options.repeatTimes = 1;
+  if (!(options.hasOwnProperty('separator'))) options.separator = '+'; 
+  if (typeof(options.separator) != 'string') options.separator = '+';
+  if (!(options.hasOwnProperty('addition'))) options.addition = ''; 
+  if (!(options.hasOwnProperty('additionRepeatTimes'))) options.additionRepeatTimes = 1; 
+  if (!(Number.isInteger(options.additionRepeatTimes) && options.additionRepeatTimes >= 0 )) options.additionRepeatTimes = 1;
+  if (!(options.hasOwnProperty('additionSeparator'))) options.additionSeparator = '';
+  if (typeof(options.additionSeparator) != 'string') options.additionSeparator = '|';
+  str = String(str) + options.addition + (options.additionSeparator + options.addition).repeat(options.additionRepeatTimes - 1);
+  str = str + (options.separator + str).repeat(options.repeatTimes - 1)
+  return str;
 };
-  
