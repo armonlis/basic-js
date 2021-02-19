adf2862f9db40612f98edebfda829fb999c819c0
Siarhei Khonski <armonius@mail.ru>
2021-02-19 15:18:49 +0300

    feat: create vigenere-cipher and test it

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,27 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct
   }
+               
+  encrypt(message, key) {
+      if (typeof(message) != 'string' || typeof(key) != 'string') throw Error('This is not message or/and key!');
+      key = key.repeat(Math.ceil(message.length / key.length));
+      key = key.toUpperCase().split('').map(el => el = el.charCodeAt(0) - 65);
+      message = message.toUpperCase().split('')
+      message.map((el, i) => el.charCodeAt(0) === 32 ? key.splice(i, 0, ' ') : el); 
+      message = message.map((el, i) => !(el.charCodeAt(0) >= 65 && el.charCodeAt(0) <= 90) ? el = el : el.charCodeAt(0) + key[i] <= 90 ? el = String.fromCharCode(el.charCodeAt(0) + key[i]) : el = String.fromCharCode(el.charCodeAt(0) + key[i] - 26));
+  return this.direct ? message.join('') : message.reverse().join('');                                    
+  }    
+  
+  decrypt(encryptedMessage, key) {
+      if (typeof(encryptedMessage) != 'string' || typeof(key) != 'string') throw Error('This is not message or/and key!');
+      key = key.repeat(Math.ceil(encryptedMessage.length / key.length));
+      key = key.toUpperCase().split('').map(el => el = el.charCodeAt(0) - 65);
+      encryptedMessage = encryptedMessage.toUpperCase().split('')
+      encryptedMessage.map((el, i) => el.charCodeAt(0) === 32 ? key.splice(i, 0, ' ') : el); 
+      encryptedMessage = encryptedMessage.map((el, i) => !(el.charCodeAt(0) >= 65 && el.charCodeAt(0) <= 90) ? el = el : el.charCodeAt(0) - key[i] >= 65 ? el = String.fromCharCode(el.charCodeAt(0) - key[i]) : el = String.fromCharCode(el.charCodeAt(0) - key[i] + 26));
+  return this.direct ? encryptedMessage.join('') : encryptedMessage.reverse().join('');                                    
+  }    
 }
 
 module.exports = VigenereCipheringMachine;
